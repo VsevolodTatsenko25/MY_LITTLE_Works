5c717cee0ae338517ae554961cb9ad4806c230e2
Vsevolod <110174765+VsevolodTatsenko25@users.noreply.github.com>
2026-02-10 11:02:56 +1000

    Delete pars2 directory

--- pars2/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://health-diet.ru/table_calorie/?utm_source=leftMenu&utm_medium=table_calorie"
-
-headers = {
-    "Accept": "*/*"
-}
-req = requests.get(url)
